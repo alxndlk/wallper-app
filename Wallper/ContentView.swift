a08f8a057c69a21c223aa86a90f922e84c4069d3
Alex <livyk@merck.com>
2025-05-27 21:57:17 +0200

    Initial Commit

---
+++ Wallper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wallper
+//
+//  Created by Livyk, Oleksandr on 27.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wallper/WallperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallperApp.swift
+//  Wallper
+//
+//  Created by Livyk, Oleksandr on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct WallperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
